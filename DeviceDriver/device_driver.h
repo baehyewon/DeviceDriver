3e8cc655778028a7937c1f0e3c0b53777a661a50
haewon <baehw1107@naver.com>
2025-07-03 16:47:11 +0900

    test device driver

--- DeviceDriver/device_driver.h
+++ DeviceDriver/device_driver.h
@@ -7,7 +7,10 @@ public:
     DeviceDriver(FlashMemoryDevice* hardware);
     int read(long address);
     void write(long address, int data);
+    void postConditionCheck(int ret, long address);
+    void preconditionCheck(long address);
 
 protected:
     FlashMemoryDevice* m_hardware;
+    int TestTryCount = 4;
 };
